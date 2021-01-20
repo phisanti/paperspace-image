b328e43ab32f4fbd63438a0083035b3dad913ddc
S. CM <33370667+phisanti@users.noreply.github.com>
2021-01-20 11:50:04 +0000

    Delete fastai-fastbook-lab directory

--- fastai-fastbook-lab/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source activate fastai
-export SHELL=/bin/bash
-mkdir -p /storage/data
-rm -rf /storage/lost+found
-ln -s /datasets/fastai/* /storage/data/
-jupyter lab --ip=0.0.0.0 --no-browser --allow-root --LabApp.trust_xheaders=True --LabApp.disable_check_xsrf=False --LabApp.allow_remote_access=True --LabApp.allow_origin='*'
