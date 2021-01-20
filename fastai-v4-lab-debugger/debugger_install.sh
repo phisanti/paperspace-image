d300ba184c6d024ef7904ea151ba9e5c466831cf
S. CM <33370667+phisanti@users.noreply.github.com>
2021-01-20 11:53:12 +0000

    Delete fastai-v4-lab-debugger directory

--- fastai-v4-lab-debugger/debugger_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source activate fastai
-jupyter labextension install @jupyterlab/debugger @jupyter-widgets/jupyterlab-manager
-jupyter nbextension enable --py widgetsnbextension
-
-rm -f /debugger_install.sh
--- fastai-v4-lab-debugger/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source activate fastai
-mkdir /storage/data
-rm -rf /storage/lost+found
-ln -s /datasets/fastai/* /storage/data/
-jupyter lab --ip=0.0.0.0 --no-browser --allow-root
